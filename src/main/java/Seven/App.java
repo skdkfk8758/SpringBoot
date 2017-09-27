e12c932851defcda4ec04f3c8f0959378b4e5ed1
CarpDm <skdkfk8758@gmail.com>
2017-09-27 15:34:37 +0900

    Update App.java2

--- src/main/java/Seven/App.java
+++ src/main/java/Seven/App.java
@@ -5,13 +5,15 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
-
+ 
+ 
+@Controller
+@EnableAutoConfiguration
 
 public class App
 {
-    @Controller
-    @EnableAutoConfiguration
-
+    @RequestMapping("/")
+    @ResponseBody
     String home() {
 
         String banner = "Riot_Practice " + "\n" + "Team is Seven \n" + " "  + "ComputerScience" + " by " + " [김동현, 이승재, 권연욱, 김은경] ";
