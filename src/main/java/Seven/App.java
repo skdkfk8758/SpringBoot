ee5788952f170c76b0352de351bb7037b1bfd3c1
EunKyeong <rornwkddl78@naver.com>
2017-09-27 14:14:39 +0900

    update app

--- src/main/java/Seven/App.java
+++ src/main/java/Seven/App.java
@@ -4,10 +4,16 @@ package Seven;
  * Hello world!
  *
  */
-public class App 
+
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        System.out.println( "Seven" );
+        System.out.println("ComputerScience");
+        System.out.println("[김동현, 이승재, 권연욱, 김은경]");
+
+
     }
 }
